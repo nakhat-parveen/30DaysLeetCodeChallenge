940377379f1a2b01e2d5e39cce9b74a3cf4c5955
Nakhat Parveen <nakhat09@gmail.com>
2026-04-26 23:24:54 +0200

    Day 4: remove duplicates solved

---
+++ leetCodeProblems/src/leetCodeProblems/RemoveDuplicates.java
@@ -0,0 +1,33 @@
+package leetCodeProblems;
+
+import java.util.Arrays;
+
+/* Given an integer array nums sorted in non-decreasing order, 
+ * remove the duplicates in-place such that each unique element appears only once. 
+ * The relative order of the elements should be kept the same.
+ * Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order. 
+The remaining elements beyond index k - 1 can be ignored.
+ */
+
+public class RemoveDuplicates {
+	
+	public static int removeDuplicates(int[] nums){
+		int k = 1;
+		for(int i=1; i <nums.length; i++) {
+			if(nums[i-1] != nums[i]) {
+				nums[k++] = nums[i];
+			}
+		}
+		return k;
+	}
+
+	public static void main(String[] args) {
+			
+		int[] nums = new int[] {1,2,2,6,8,8,9};
+		System.out.println("Length of the array without any dupliacted values: " + removeDuplicates(nums));
+		
+	}
+
+}
